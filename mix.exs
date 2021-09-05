68b75e97a2dc4a31cad1a6798524291b3f424f1e
Pedro Marques da Silva <posilva@gmail.com>
2021-09-05 14:53:42 +0100

    Enable OS Monitor

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Liveopex.MixProject do
   def application do
     [
       mod: {Liveopex.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
